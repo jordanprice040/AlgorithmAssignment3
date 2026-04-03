cc8aac2e0feca6b8f10dbe912a54c3872e74c3c2
jordanprice040 <jordanprice@ufl.edu>
2026-04-02 22:08:30 -0400

    outline

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Store result of DP
+struct Result {
+    long long maxValue;
+    string subsequence;
+};
+
+// Function that solves HVLCS
+Result computeHVLCS(const string& A, const string& B,
+                    const unordered_map<char, int>& valueMap) {
+    // 1. Create DP table
+    // dp[i][j] = maximum value of a common subsequence
+    //            between A[0..i-1] and B[0..j-1]
+
+    // 2. Fill DP table using recurrence
+
+    // 3. Reconstruct one optimal subsequence by walking backward
+
+    // 4. Return both maximum value and subsequence
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // hard code example
+
+    // Call computeHVLCS(...)
+    // Print max value
+    // Print subsequence
 }
